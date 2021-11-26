6dce70b9dda72706b182fe55def24baa7b97a44a
milske <millanerg@gmail.com>
2021-11-26 11:15:04 +0100

    Adding API forecast in JS.

--- src/index.js
+++ src/index.js
@@ -14,25 +14,27 @@ let days = [
   "Saturday",
 ];
 
-function showForecast() {
+function showForecast(response) {
+  let forecastDays = response.data.daily;
+
   let forecast = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
+
+  forecastDays.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       ` 
               <div class="col-2">
-                <div class="forecast-day">${day}</div>
+                <div class="forecast-day">${forecastDay.dt}</div>
                 <img
-                  src="https://openweathermap.org/img/wn/04n@2x.png"
+                  src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                   alt="forecast-pic"
                   width="40"
                 />
                 <div class="forecast-temperatures">
-                  <span class="temperature-max"> 18 </span>
-                  <span class="temperature-min"> 12 </span>
+                  <span class="temperature-max"> ${forecastDay.temp.max} </span>
+                  <span class="temperature-min"> ${forecastDay.temp.min} </span>
                 </div>
               </div>
             `;
@@ -43,6 +45,13 @@ function showForecast() {
 
 let weekday = days[day];
 currentDate.innerHTML = `${weekday} ${hours}:${minutes}`;
+
+function getForecast(coordinates) {
+  let apiKey = "e9749c87f79d989e0dfa640ff0c29863";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showForecast);
+}
 
 function showWeather(response) {
   document.querySelector("#currentCity").innerHTML = response.data.name;
@@ -63,6 +72,8 @@ function showWeather(response) {
     );
 
   celciusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
@@ -97,5 +108,3 @@ celciusLink.addEventListener("click", showCelciusTemperature);
 
 let search = document.querySelector("#search");
 search.addEventListener("submit", searchCity);
-
-showForecast();
